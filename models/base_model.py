cf628c98ad5bb254f1956ee56ae163405a583292
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-11 21:45:58 +0000

    Add task #2 corrections

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models.__init__ import storage
+        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
