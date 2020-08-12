495c7b654a0fadb0c5767dcfc1800537aa3a49ac
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-12 04:05:12 +0000

    Add task #4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- SQL script prepare MySQL for the test environment
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_test_pwd';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
