2abf261b5fcfc116e64b363d21b3db2879afc2ea
bryano13 <bryano13@hotmail.com>
2020-08-13 02:44:32 -0500

    task 7, user.py

--- models/user.py
+++ models/user.py
@@ -8,3 +8,10 @@ from models.review import Review
 
 class User(BaseModel, Base):
     """Class user with it's attributes"""
+
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
