a202e3bcee8e5aac6215804428b0a13574811408
bryano13 <bryano13@hotmail.com>
2020-08-13 02:21:36 -0500

    db_storage commented

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""New engine for saving data: DBStorage"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.state import State
