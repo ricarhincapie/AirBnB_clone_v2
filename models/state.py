c85ac4f6293ff9f503bddf05c6f8be4f517c2558
bryano13 <bryano13@hotmail.com>
2020-08-13 02:16:16 -0500

    init, city.py, state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from os import getenv
+from models.city import City
+from sqlalchemy import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
