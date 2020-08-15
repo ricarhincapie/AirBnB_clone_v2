5c746dc97af77a6583fc2ecb5e2f76eacc21303b
bryano13 <bryano13@hotmail.com>
2020-08-15 00:21:05 -0500

    changes in place.py and amenity.py

--- models/place.py
+++ models/place.py
@@ -1,62 +1,40 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from models import city, user, review
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy.orm import relationship, backref
+from models.review import Review
 from os import getenv
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = "places"
+    __tablename__ = 'places'
 
     city_id = Column(String(60),
                      ForeignKey('cities.id'),
                      nullable=False)
-
     user_id = Column(String(60),
                      ForeignKey('users.id'),
                      nullable=False)
-
-    name = Column(String(128),
+    name = Column(String(60),
                   nullable=False)
-
     description = Column(String(1024),
-                         default="",
                          nullable=True)
-
     number_rooms = Column(Integer,
                           nullable=False,
                           default=0)
-
     number_bathrooms = Column(Integer,
                               nullable=False,
                               default=0)
-
     max_guest = Column(Integer,
                        nullable=False,
                        default=0)
-
     price_by_night = Column(Integer,
                             nullable=False,
                             default=0)
-
-    latitude = Column(Float)
-
-    longitude = Column(Float)
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        reviews = relationship("Review",
-                               cascade="all, delete",
-                               backref="place")
-    else:
-        @property
-        def reviews(self):
-            """ """
-            storage = Base.all(review.Review)
-            my_list = []
-            for instance in storage:
-                if instance.place_id == self.id:
-                    my_list.append(instance)
-            return my_list
+    latitude = Column(Float,
+                        nullable=True)
+    longitude = Column(Float,
+                        nullable=True)
