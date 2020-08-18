df4a126e96a1baa1b1b3fa6c31011cd9b62ae9d4
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-18 04:38:28 +0000

    Add task #1 and #2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Generates a .tgz archive"""
+
+from fabric.api import run, put, env
+from os.path import exists
+
+env.user = 'ubuntu'
+env.hosts = ['34.75.248.42', '54.234.64.157']
+
+
+def do_deploy(archive_path):
+    """
+        deploy the archive to the webservers
+    """
+    if exists(archive_path) is False:
+        return False
+
+    filename_wo_ext = archive_path[9:34]
+    filename_w_ext = archive_path[9:]
+    input_path = "/data/web_static/releases/{}/".format(filename_wo_ext)
+
+    try:
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(input_path))
+        run("sudo tar -zxvf /tmp/{} -C {}".format(filename_w_ext, input_path))
+        run("sudo rm -rf /tmp/{}".format(filename_w_ext))
+        run("sudo mv -n {}/web_static/* {}".format(input_path, input_path))
+        run("sudo rm -rf {}/web_static".format(input_path))
+        run("sudo rm /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(input_path))
+        return True
+
+    except Exception:
+        return False
