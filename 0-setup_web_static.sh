4b430ced80191da8ab06adf766eb518e256dc5a7
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-08-18 04:35:23 +0000

    Task #0 correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
-# Script to get a machine ready for static deploy with Fabric and Nginx
-
-sudo apt-get -y update
-sudo apt-get -y install nginx
+# sets up your web servers for the deployment of web_static
+apt-get -y update
+apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 
-sudo mkdir -p ~/data/web_static/{releases/test,shared}
-str_html=$'<html>\n\t<head>\n\t</head>\n\t<body>\n\tHolberton School\n\t</body>\n</html>'
-echo "$str_html" | sudo tee ~/data/web_static/releases/test/index.html
-sudo ln -s ~/data/web_static/releases/test/ ~/data/web_static/current
-sudo chown -R ubuntu:ubuntu ~/data/
-sudo sed -i '/listen 80 default_server/a \\n\tlocation /hbnb_static/ {\n\t\talias /home/ubuntu/data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+
+echo "<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset='UTF-8'>
+	<title>Test</title>
+  </head>
+  <body>
+    <p>Nginx html test file</p>
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
 exit 0
