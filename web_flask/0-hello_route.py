d07ededf9dc6b79d8b2cdb11d50c82398585d69a
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-02 19:21:56 +0000

    Add task #0 corrections

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Module to start a Flask web application
 """
 
@@ -10,6 +10,8 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def start_app():
+    """Routes the / GET request
+    """
     return 'Hello HBNB!'
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
